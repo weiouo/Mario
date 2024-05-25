555aece16540be1cb8e6423622a6a3149b4263f7
Y-shiuan <161366542+Y-shiuan@users.noreply.github.com>
2024-05-25 21:09:21 +0800

    Update Block.java

--- SuperMario/src/com/game/obj/Block.java
+++ SuperMario/src/com/game/obj/Block.java
@@ -3,15 +3,22 @@ package com.game.obj;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
+import com.game.graph.Texture;
+import com.game.main.Game;
 import com.game.obj.util.Handler;
 import com.game.obj.util.ObjID;
 
 public class Block extends GameObj{
-
+	private Texture tex = Game.getTexture();
+	private int index;
+	private BufferedImage[] sprite;
 	
-	public Block (int x, int y, int width, int height, int scale,Handler handler) {
+	public Block (int x, int y, int width, int height, int scale, Handler handler, int index) {
 		super(x, y, ObjID.Block, width, height, scale, handler);
+		this.index = index;
+		sprite = tex.getTile1();
 	}
 
 	@Override
@@ -21,8 +28,10 @@ public class Block extends GameObj{
 	@Override
 	public void render(Graphics gf) {
 		//temporary code
-		gf.setColor(Color.WHITE);
-		gf.drawRect((int) get_x(), (int) get_y(), (int) get_width(), (int)get_height());
+		//gf.setColor(Color.WHITE);
+		//gf.drawRect((int) get_x(), (int) get_y(), (int) get_width(), (int)get_height());
+
+		gf.drawImage(sprite[index], (int) get_x(), (int) get_y(), (int) get_width(), (int) get_height(), null);
 	}
 
 	@Override
