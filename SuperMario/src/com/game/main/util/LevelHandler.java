b76c1934af8b97e03fc445e15d85387689ba6918
Y-shiuan <161366542+Y-shiuan@users.noreply.github.com>
2024-06-02 06:42:26 +0800

    Update LevelHandler.java

--- SuperMario/src/com/game/main/util/LevelHandler.java
+++ SuperMario/src/com/game/main/util/LevelHandler.java
@@ -4,9 +4,11 @@ import java.awt.image.BufferedImage;
 
 import com.game.graph.BufferedImageLoader;
 import com.game.obj.Block;
+import com.game.obj.Cloud;
 import com.game.obj.Flag;
 import com.game.obj.FlagPole;
 import com.game.obj.Goomba;
+import com.game.obj.Mountain;
 import com.game.obj.Pipe;
 import com.game.obj.Player;
 import com.game.obj.util.Handler;
@@ -26,6 +28,7 @@ public class LevelHandler {
 	
 	public void start() {
 		setLevel(PARENT_FOLDER + "/1_1.png");
+		loadBackground(PARENT_FOLDER + "/1_1b.png");
 		loadCharacters(PARENT_FOLDER + "/1_1c.png");	
 	}
 	public void restart() {
@@ -97,6 +100,52 @@ public class LevelHandler {
 				} 
 			}
 		}
+	}
+
+	private void loadBackground(String levelBackgroundPath) {
+		this.levelTiles = loader.loadImage(levelBackgroundPath);
+		
+		int width = levelTiles.getWidth();
+		int height = levelTiles.getHeight();
+		
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				int pixel = levelTiles.getRGB(i, j);
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = pixel & 0xff;
+				
+				if (red == 255 && green == 255 && blue == 255) {
+					continue;
+				}
+				if (red == 50 && green == 250 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 0));//左邊山
+				} else if (red == 0 && green == 250 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 1));//上面山
+				} else if (red == 0 && green == 250 && blue == 50) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 2));//右邊山
+				} else if (red == 100 && green == 250 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 3));//左下山
+				} else if (red == 0 && green == 200 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 4));//中下山
+				} else if (red == 0 && green == 250 && blue == 100) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 5));//右下山
+				} else if (red == 50 && green == 255 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 6));//左邊草
+				} else if (red == 0 && green == 255 && blue == 0) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 7));//中間草
+				} else if (red == 0 && green == 255 && blue == 50) {
+					handler.addObj(new Mountain(i*16, j*16, 16, 16, 3, handler, 8));//右邊草
+				} else if (red == 50 && green == 0 && blue == 255) {
+					handler.addObj(new Cloud(i*16, j*16, 16, 32, 3, handler, 0));//左邊雲
+				} else if (red == 0 && green == 0 && blue == 255) {
+					handler.addObj(new Cloud(i*16, j*16, 16, 32, 3, handler, 1));//中間雲
+				} else if (red == 0 && green == 50 && blue == 255) {
+					handler.addObj(new Cloud(i*16, j*16, 16, 32, 3, handler, 2));//右邊雲
+				}
+			}
+		}
+		
 	}
 	
 }
